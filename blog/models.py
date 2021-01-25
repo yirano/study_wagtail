bcf24da70ec08436865fef9c50e3308065c7128a
Yirang Tiffany Arellano <yirang@mostlyserious.io>
2021-01-25 12:40:44 -0500

    Link blog post tags to show all articles of same tag

--- blog/models.py
+++ blog/models.py
@@ -89,3 +89,16 @@ class BlogPageGalleryImage(Orderable):
         ImageChooserPanel('image'),
         FieldPanel('caption')  # Being imported with the argument from line 50
     ]
+
+
+class BlogTagIndexPage(Page):
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
